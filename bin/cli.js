581f5c7b1f794d71d2003f9e1749cca43a33b3d5
Sean <seanegjwinter@gmail.com>
2026-02-20 18:35:13 +0800

    Rewrite CLI with switch routing + dynamic version

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const path = require("path");
+const pkg = require("../package.json");
 
 const command = process.argv[2];
 
@@ -9,30 +10,48 @@ if (!command) {
   process.exit(0);
 }
 
-if (command === "doctor") {
-  require(path.join(__dirname, "doctor"));
-} else if (command === "install") {
-  require(path.join(__dirname, "install"));
-} else if (command === "init") {
-  require(path.join(__dirname, "init"));
-} else if (command === "revert") {
-  require(path.join(__dirname, "revert"));
-} else if (command === "--version" || command === "-v") {
-  // Keep this in sync with package.json version (manual for now)
-  console.log("openclaw-deterministic v0.1.0");
-  process.exit(0);
-} else {
-  console.error(`Unknown command: ${command}\n`);
-  showHelp();
-  process.exit(1);
+switch (command) {
+  case "doctor":
+    require(path.join(__dirname, "doctor"));
+    break;
+
+  case "install":
+    require(path.join(__dirname, "install"));
+    break;
+
+  case "init":
+    require(path.join(__dirname, "init"));
+    break;
+
+  case "revert":
+    require(path.join(__dirname, "revert"));
+    break;
+
+  case "--version":
+  case "-v":
+    console.log(`openclaw-deterministic v${pkg.version}`);
+    process.exit(0);
+    break;
+
+  case "--help":
+  case "-h":
+    showHelp();
+    process.exit(0);
+    break;
+
+  default:
+    console.error(`Unknown command: ${command}\n`);
+    showHelp();
+    process.exit(1);
 }
 
 function showHelp() {
-  console.log("OpenClaw Deterministic CLI\n");
+  console.log("\nOpenClaw Deterministic CLI\n");
   console.log("Usage:");
   console.log("  oc-deterministic init");
   console.log("  oc-deterministic doctor");
   console.log("  oc-deterministic install");
   console.log("  oc-deterministic revert");
-  console.log("  oc-deterministic --version\n");
+  console.log("  oc-deterministic --version");
+  console.log("  oc-deterministic --help\n");
 }
